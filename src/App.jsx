9f5bd870636ed1fdf1f13fc84b98187c6f62ebac
manhoosbilli1 <shoaib.mustafa7@hotmail.com>
2025-07-26 17:12:41 +0400

    this is a fresh start

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home/Home';
-import Products from './Pages/Products/Products';
-import Navbar from './components/Navbar/Navbar'; 
-import Blogs from './Pages/Blogs/Blogs';
-import BlogsDetails from './components/BlogsDetails/BlogsDetails';
-
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/blogs/:id" element={<BlogsDetails />} /> 
-      </Routes>
-    </Router>
-  );
+  return <>Hello world!</>;
 }
 
 export default App;
