33464fb9e7f9bcd67e57842b858cdcee123cd9d1
KamranTalpur <kamrantalpur646@gmail.com>
2025-10-05 05:31:32 +0500

    Website Made Fully Mobile Responsive.

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -3,7 +3,7 @@ export default function Contact() {
     <section className="contact-section min-h-screen bg-kindofwhite flex items-center justify-center">
       <div className="container mx-auto px-4 text-center md:px-6">
         <h1 className="text-4xl text-sexyblue font-domine md:text-2xl">
-          Contact Us
+          Contact Us.
         </h1>
       </div>
     </section>
